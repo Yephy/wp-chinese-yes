a58badbfa1d6d8b3bbac8ff41f1c974a344865c0
sunxiyuan <sxy@ibadboy.net>
2020-05-12 19:07:28 +0800

    输出的翻译结果按文本域-zh_CN.[mo|po]命名

--- cli.py
+++ cli.py
@@ -6,5 +6,5 @@ from potrans import Translator
 t = Translator(pot.get(sys.argv[1]))
 t.go_translate(plugin_info_handle.get_name())
 
-t.save_po_file("./out/zh_CN.po")
-t.save_mo_file("./out/zh_CN.mo")
+t.save_po_file("./out/%s-zh_CN.po" % (plugin_info_handle.get_text_domain()))
+t.save_mo_file("./out/%s-zh_CN.mo" % (plugin_info_handle.get_text_domain()))
