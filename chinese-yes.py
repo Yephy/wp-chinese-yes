eb6304063c1fc173ea9e63a2389f2c58c29e16b6
sunxiyuan <sxy@ibadboy.net>
2020-05-13 20:55:23 +0800

    更新正则表达式

--- chinese-yes.py
+++ chinese-yes.py
@@ -175,8 +175,11 @@ class Translator:
                 http_client.close()
 
         exclude_dict = {}
+        #match_list = re.findall(
+        #    r"%[sd]|&[a-z]+;|%[0-9]+{\$s}|\[.+\]|<.+?>|http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+        #    text)
         match_list = re.findall(
-            r"%[sd]|&[a-z]+;|%[0-9]+{\$s}|\[.+\]|<.+?>|http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
+            r"\[.+\]|http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+",
             text)
         if match_list is not None:
             for value in match_list:
